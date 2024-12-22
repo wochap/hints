8709b8acf5eb9fe23f30a609a818e6ce8d1e2176
alfredosequeida <alfredosequeida@outlook.com>
2024-12-21 21:19:39 -0800

    added function for getting active window

--- hints/platform_utils/linux/window_manager.py
+++ hints/platform_utils/linux/window_manager.py
@@ -13,6 +13,14 @@ class WindowManager:
     def __init__(self):
         self.screen = Wnck.Screen.get_default()
 
+    def get_active_window(self) -> Wnck.Window:
+        """Get active window.
+
+        :return: active window.
+        """
+        self.screen.force_update()
+        return self.screen.get_active_window()
+
     def get_window_extents(self, pid: int) -> tuple[int, int, int, int] | None:
         """Get Window extents by window pid.
 
