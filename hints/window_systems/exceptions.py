b91aa2639b0c8bdc570c11a45741193f3568e5ea
alfredosequeida <alfredosequeida@outlook.com>
2025-01-25 22:36:35 -0800

    Adds Atspi fallback for non-EWMH window managers.

--- hints/window_systems/exceptions.py
+++ hints/window_systems/exceptions.py
@@ -33,3 +33,31 @@ class WindowSystemNotSupported(Exception):
             f" {", ".join(self.supported_wms)}. To request support open an"
             " issue: https://github.com/AlfredoSequeida/hints/issues."
         )
+
+
+class CouldNotFindWindowInformation(Exception):
+    """Could not find window information."""
+
+    def __init__(self, information_identifier: str):
+        """Exception constructor.
+
+        :param information_identified: A string identifier for the type
+            of information that could not be found.
+        """
+        self.information_identifier = information_identifier
+
+    def __str__(self) -> str:
+        return f"Could not find {self.information_identifier} for the current window."
+
+
+class CouldNotIdentifyActiveWindow(Exception):
+    """Could not identify active window."""
+
+    def __str__(self) -> str:
+        return (
+            "Could not find an active window. If you are using a"
+            " Window Manager that is not EWMH compliant (such as DWM), you"
+            " will be limited to applications that are supported by Atspi."
+            " So you might not get hints on applications like terminals or"
+            " applications that use GUI Frameworks without Atspi support."
+        )
--- hints/window_systems/x11.py
+++ hints/window_systems/x11.py
@@ -2,11 +2,20 @@
 
 from gi import require_version
 
+require_version("Atspi", "2.0")
 require_version("Wnck", "3.0")
 
-from gi.repository import Wnck
+from enum import Enum
 
+from gi.repository import Atspi, Wnck
+from hints.window_systems.exceptions import (CouldNotFindWindowInformation,
+                                             CouldNotIdentifyActiveWindow)
 from hints.window_systems.window_system import WindowSystem
+
+
+class WindowStrategy(Enum):
+    WNCK = 0
+    ATSPI = 1
 
 
 class X11(WindowSystem):
@@ -17,7 +26,46 @@ class X11(WindowSystem):
 
         self.screen = Wnck.Screen.get_default()
         self.screen.force_update()
-        self.active_window = self.screen.get_active_window()
+        self.active_window, self.window_strategy = self._get_active_window()
+
+    def _get_active_window(
+        self,
+    ) -> tuple[Wnck.Window | Atspi.Accessible | None, WindowStrategy]:
+        """Get active window.
+
+        This method attempts to get the active window using libwnck.
+        However, some window managers like DWM are not EWMH compliant
+        and libwnck will not be able to get the active window. So as a
+        fallback, we use Atspi.
+
+        :return: The active window and the window strategy used to get
+            the active window (WNCK or ATSPI).
+        """
+
+        active_window = self.screen.get_active_window()
+        window_strategy = WindowStrategy.WNCK
+
+        if not active_window:
+            desktop = Atspi.get_desktop(0)
+            for app_index in range(desktop.get_child_count()):
+                window = desktop.get_child_at_index(app_index)
+                # Gnome creates a mutter application that is also focused.
+                # This is not what we want, so we are skipping it.
+                if "mutter-x11-frames" in window.get_description():
+                    continue
+                for window_index in range(window.get_child_count()):
+                    current_window = window.get_child_at_index(window_index)
+                    if current_window.get_state_set().contains(Atspi.StateType.ACTIVE):
+                        active_window = current_window
+                        window_strategy = WindowStrategy.ATSPI
+                        break
+                if active_window:
+                    break
+
+        if not active_window:
+            raise CouldNotIdentifyActiveWindow()
+
+        return active_window, window_strategy
 
     @property
     def window_system_name(self) -> str:
@@ -35,7 +83,19 @@ class X11(WindowSystem):
 
         :return: Active window extents (x, y, width, height).
         """
-        return self.active_window.get_geometry()
+        res = None
+
+        match (self.window_strategy):
+            case WindowStrategy.WNCK:
+                res = self.active_window.get_geometry()
+            case WindowStrategy.ATSPI:
+                extents = self.active_window.get_extents(Atspi.CoordType.SCREEN)
+                res = (extents.x, extents.y, extents.width, extents.height)
+
+        if not res:
+            raise CouldNotFindWindowInformation("window extents")
+
+        return res
 
     @property
     def focused_window_pid(self) -> int:
@@ -43,7 +103,18 @@ class X11(WindowSystem):
 
         :return: Process ID of focused window.
         """
-        return self.active_window.get_pid()
+        res = None
+
+        match (self.window_strategy):
+            case WindowStrategy.WNCK:
+                res = self.active_window.get_pid()
+            case WindowStrategy.ATSPI:
+                res = self.active_window.get_process_id()
+
+        if not res:
+            raise CouldNotFindWindowInformation("process id")
+
+        return res
 
     @property
     def focused_applicaiton_name(self) -> str:
@@ -54,4 +125,15 @@ class X11(WindowSystem):
 
         :return: Focused application name.
         """
-        return self.active_window.get_class_instance_name()
+        res = ""
+
+        match (self.window_strategy):
+            case WindowStrategy.WNCK:
+                res = self.active_window.get_class_instance_name()
+            case WindowStrategy.ATSPI:
+                res = self.active_window.get_application().get_name()
+
+        if not res:
+            raise CouldNotFindWindowInformation("application name")
+
+        return res
