31b78c56a69b1fc5f22ce7648f1129ae71147977
alfredosequeida <alfredosequeida@outlook.com>
2025-02-16 21:40:04 -0800

    mend
    Adds custom mouse solution.

--- setup.py
+++ setup.py
@@ -1,31 +1,98 @@
 import codecs
-import os
-import re
+from os import getenv, path
+from pathlib import Path
+from re import M, search
+from subprocess import run
 
 from setuptools import setup
+from setuptools.command.install import install
+
+
+class CouldNotFindExpectedInstallDirectory(Exception):
+    def __str__(self):
+        return (
+            "Could not find expected install directory. This is required to"
+            " setup the hints-mouse service. If you are not using PIPX for"
+            " installation, set the 'HINTS_EXPECTED_BIN_DIR' environment"
+            " variable with the location hints will be installed in. For"
+            " example:"
+            " 'export HINTS_EXPECTED_BIN_DIR=\"$HOME/.local/bin\"'."
+            " Then attemp to re-install."
+        )
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-here = os.path.abspath(os.path.dirname(__file__))
+here = path.abspath(path.dirname(__file__))
 
 
 def read(*parts):
-    with codecs.open(os.path.join(here, *parts), "r") as fp:
+    with codecs.open(path.join(here, *parts), "r") as fp:
         return fp.read()
 
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    version_match = search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
 
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def get_bin_dir(self):
+        bin_dir = ""
+
+        try:
+            pipx_bin_dir_cmd = run(
+                ["pipx", "environment", "--value", "PIPX_BIN_DIR"],
+                check=True,
+                capture_output=True,
+            )
+            bin_dir = pipx_bin_dir_cmd.stdout.decode("utf-8").strip()
+        except FileNotFoundError:
+            bin_dir = getenv("HINTS_EXPECTED_BIN_DIR", "")
+
+        if not bin_dir:
+            raise CouldNotFindExpectedInstallDirectory()
+
+        return bin_dir
+
+    def install_hints_mouse_service(self):
+        """Install hints-mouse service for the current user."""
+        bin_dir = self.get_bin_dir()
+        if bin_dir:
+            user_service_directory = Path(path.expanduser("~")) / ".config/systemd/user"
+            user_service_directory.mkdir(parents=True, exist_ok=True)
+            with open(
+                user_service_directory / "hints-mouse.service",
+                mode="w+",
+                encoding="utf-8",
+            ) as service_file:
+                service_file.write(
+                    "[Unit]\n"
+                    "Description=Hints mouse service\n"
+                    "[Service]\n"
+                    "Type=simple\n"
+                    f"ExecStart={bin_dir}/hints-mouse\n"
+                    "Restart=always\n"
+                    "Environment=XDG_SESSION_TYPE=$XDG_SESSION_TYPE\n"
+                    "[Install]\n"
+                    "WantedBy=default.target\n"
+                )
+            run(["systemctl", "--user", "daemon-reload"], check=False)
+
+    def run(self):
+        install.run(self)
+        self.install_hints_mouse_service()
+
+
 dynamic_version = find_version("hints", "__init__.py")
 
-setup(
+s = setup(
     name="hints",
     version=dynamic_version,
     author="Alfredo Sequeida",
@@ -62,6 +129,12 @@ setup(
     license="GPLv3",
     packages=["hints", "hints.backends", "hints.huds", "hints.window_systems"],
     include_package_data=True,
-    install_requires=["PyGObject", "pillow", "pyscreenshot", "opencv-python"],
-    entry_points={"console_scripts": ["hints = hints.hints:main"]},
+    install_requires=["PyGObject", "pillow", "pyscreenshot", "opencv-python", "evdev"],
+    entry_points={
+        "console_scripts": [
+            "hints = hints.hints:main",
+            "hints-mouse = hints.mouse_service:main",
+        ]
+    },
+    cmdclass={"install": PostInstallCommand},
 )
